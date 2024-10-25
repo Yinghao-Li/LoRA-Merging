bca6e3da8d3685b80d9a893052a4db4ea48ea1dd
Yinghao-Li <www6com2010@hotmail.com>
2024-10-25 17:34:27 -0400

    update mathqa evaluation

--- src/eval_funcs.py
+++ src/eval_funcs.py
@@ -38,6 +38,37 @@ class EvalFuncs:
         return report
 
     @staticmethod
+    def mathqa(df):
+        n_correct = 0
+        correct_ids = []
+        incorrect_ids = []
+        for idx, pred_str, ref_str in zip(df["idx"], df["generated"], df["response"]):
+            try:
+                pred = re.search(r"So the answer is\s*(.+)", pred_str).group(1).strip()
+            except AttributeError:
+                pred = "missing"
+
+            ref = re.search(r"So the answer is\s*(.+)", ref_str).group(1).strip()
+
+            if pred == ref:
+                correct_ids.append(idx)
+                n_correct += 1
+            else:
+                incorrect_ids.append(idx)
+
+        accuracy = n_correct / len(df)
+
+        report = {
+            "metrics": {
+                "accuracy": accuracy,
+            },
+            "incorrect_ids": incorrect_ids,
+            "correct_ids": correct_ids,
+        }
+
+        return report
+
+    @staticmethod
     def math(df):
         n_correct = 0
         correct_ids = []
