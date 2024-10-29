69911817b5e89158712a3286b9345cd7effce1fa
Yinghao-Li <www6com2010@hotmail.com>
2024-10-29 15:23:52 -0400

    udpate data processing script

--- scripts/process-data.sh
+++ scripts/process-data.sh
@@ -3,3 +3,4 @@ PYTHONPATH="." python ./data-processing/gsm8k_.py
 PYTHONPATH="." python ./data-processing/math_.py
 PYTHONPATH="." python ./data-processing/mathqa_.py
 PYTHONPATH="." python ./data-processing/svamp_.py
+PYTHONPATH="." python ./data-processing/omnimath_.py
